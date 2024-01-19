2c854b10223a13322f92d43b764e4e3fc938aa51
albinsony95 <albinsony8@gmail.com>
2024-01-19 06:42:50 -0600

    JSON file added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import axios from 'axios';
+//import './App.scss';
+import { useState, useEffect } from 'react';
 
 function App() {
+  // const url="https://ddragon.leagueoflegends.com/cdn/14.1.1/data/en_US/champion.json";
+  const url = "http://localhost:2024/champions";
+  const [ChampionContent,setChampionContent]=useState(null);
+  const [selectedChampion,setSelectedChampion]=useState(null);
+  let arr=[];
+  useEffect (() => {
+    axios.get(url).then((res)=> {
+      console.log(res.data.data);
+
+      const obj = res.data.data;
+      for ( const prop in obj){
+        console.log(obj[prop]);
+        arr.push(obj[prop]);
+      }
+      console.log(arr[0].image.full);
+
+
+
+
+      // arr= res.data.forEach(element => {
+      //   console.log(element);
+      //   arr.push(element);
+      // });
+      // console.log(arr);
+
+      setChampionContent(res);
+      //setSelectedChampion(res.data.data[0].id);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  },[]);
   return (
-    <div className="App">
-     <h1>Paired Programming</h1>
-    </div>
+<h1> league of legends</h1>
+
+
   );
 }
 
