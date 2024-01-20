cf418a28aa6878f491eb438751c53fe3120efc5a
KingChoices <kingchoicesyt@gmail.com>
2024-01-19 22:08:44 -0500

    - fixed duplicate objects - added card components - added page structure - added sass library - fixed infinite looping from console logging array

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
+import axios from "axios";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Card from "./components/Card";
+import Home from "./Display/Home.js";
+//import data from "./data/test.json";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [championContent, setChampionContent] = useState();
+  const [selectedChampion, setSelectedChampion] = useState();
+
+  const url =
+    "https://ddragon.leagueoflegends.com/cdn/14.1.1/data/en_US/champion.json";
+
+  const dataArr = [];
+
+  useEffect(() => {
+    //fetch data from url
+    axios.get(url).then((res) => {
+      //logging data in console
+      //console.log(res.data.data);
+      //re-arranging data
+      const objects = res.data.data;
+      for (const prop in objects) {
+        //console.log(objects[prop]);
+        dataArr.push(objects[prop]);
+        setData(dataArr);
+        //console.log(dataArr);
+      }
+    });
+  }, []);
+
+  console.log(data);
+  //data.splice(0, 166);
+
+  //console.log(dataArr[0].image.full);
+  //console.log(dataArr);
+
   return (
-    <div className="App">
-     <h1>Paired Programming</h1>
-    </div>
+    <BrowserRouter className="App">
+      <Routes>
+        <Route
+          path="/"
+          element={<Home championContent={championContent} data={data} />}
+        />
+        <Route
+          path="/:champId"
+          element={<Home championContent={championContent} data={data} />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Card.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Card = (props) => {
+  const link = "https://ddragon.leagueoflegends.com/cdn/14.1.1/img/champion/";
+  return (
+    <Link className="card__container">
+      <div className="card__container--title">
+        <h2>{props.title}</h2>
+      </div>
+      <div className="card__container--grid">
+        <div className="card__caontainer--image">
+          <img src={link + props.image} alt="champion icon" />
+        </div>
+        <div className="card__container--info">
+          <h3>{props.name}</h3>
+          <p>{props.blurb}</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default Card;
